fa45e1308bae31aabb671f93770f94f734738c50
will-mize-tm <will.mize@toyota.com>
2021-11-24 15:54:41 -0600

    removed build files to prevent merge conflicts

--- typeScript/util/constructVlpUrl.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.constructVlpUrl = void 0;
-// VLP URL construction function
-const protractor_1 = require("protractor");
-function constructVlpUrl() {
-    return protractor_1.browser.params.url + '?dealerCd=' + protractor_1.browser.params.dealerCd + '&source=' + protractor_1.browser.params.source;
-}
-exports.constructVlpUrl = constructVlpUrl;
