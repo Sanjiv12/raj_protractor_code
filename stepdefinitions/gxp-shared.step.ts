dbfb2b95c327600f09c7462eceb460069c538ff6
Riley Byars <87771005+riley-byars-tm@users.noreply.github.com>
2021-12-01 13:26:48 -0600

    Feature/fix assertion reference (#35)
    * change capitalization of assertion
    
    * remove commented line
    
    * change back to single line for sign in definition

--- stepdefinitions/gxp-shared.step.ts
+++ stepdefinitions/gxp-shared.step.ts
@@ -12,92 +12,27 @@ import { SavesPageRedesign } from "../pages/savesPageRedesign";
 import { constructSavePageUrl } from "../util/constructSavePageUrl";
 import { getPageInfo } from "../util/getPageInfo";
 import { waitForVisibilityOf } from "../util/waitForVisibilityOf";
-import { Assertion } from "../util/Assertion";
+import { Assertion } from "../util/assertion";
 import { CreateAccountPage } from "../pages/createAccountPage";
 
-let createAccountPage : CreateAccountPage = new CreateAccountPage();
-const savesPage : SavesPageRedesign = new SavesPageRedesign();
-let navMenu : NavMenu = new NavMenu();
+let createAccountPage: CreateAccountPage = new CreateAccountPage();
+const savesPage: SavesPageRedesign = new SavesPageRedesign();
+let navMenu: NavMenu = new NavMenu();
 let until = protractor.ExpectedConditions;
 
 let MAX_TIME_WAIT = 10000;
 
- /**
-  * Setup Section
-  *
-  * steps for consistent setup before features are executed (login included)
-  */
+/**
+ * Setup Section
+ *
+ * steps for consistent setup before features are executed (login included)
+ */
 
- /**
-  * Teardown Section
-  *
-  * steps for consistent tear down once feature execution is complete
-  */
-
- /**
-  * Navigations
-  *
-  * shared navigations to important pages
-  */
-
- /**
-  * Common Actions? / Common Accounts?
-  *
-  * things like save a vehicle, create a deal, etc.
-  */
-
-  async function hasNotPreviouslyLoggedIn() {
-    await browser.driver.sleep(15*1000);
-    return browser.driver.getCurrentUrl().then((url) => {
-        return url.includes('account.toyota.com');
-    });
- }
-
- When(/User Signs In(\s\"(.*?)\")?(\s\"(.*?)\")?/, async(email?: string, password?: string) =>{
-     await browser.driver.wait(
-         until.visibilityOf(navMenu.profileIcon),
-         MAX_TIME_WAIT,
-         'Top Nav Profile Icon taking too long to appear in the DOM'
-     );
-     navMenu.profileIcon.click();
-     await browser.driver.wait(
-         until.visibilityOf(
-             navMenu.dgComponentMenuDropdownDesktop
-         ),
-         MAX_TIME_WAIT,
-         'Dropdown Element taking too long to appear in the DOM'
-     );
-     await navMenu.dgLoginButton.click();
-     if (await hasNotPreviouslyLoggedIn()) {
-         await createAccountPage.userName.sendKeys(email ? email : browser.params.caemailreg);
-         await createAccountPage.nextStepButton.click();
-         await createAccountPage.userPwd.sendKeys(password ? password : browser.params.capwdreg);
-         await createAccountPage.signInButton.click();
-     }
- });
-
- Given('User is not logged in to account', async () => {
-    await browser.driver.wait(
-        until.visibilityOf(navMenu.profileIcon),
-        MAX_TIME_WAIT,
-        'Top Nav Profile Icon taking too long to appear in the DOM'
-    );
-    // browser.executeScript("arguments[0].click();", navMenu.profileIcon);
-    navMenu.profileIcon.click();
-
-    await browser.driver.wait(
-        until.visibilityOf(
-            navMenu.dgComponentMenuDropdownDesktop
-        ),
-        MAX_TIME_WAIT,
-        'Dropdown Element taking too long to appear in the DOM'
-    );
-    if (!Assertion.expect(await navMenu.dgLoginButton.isDisplayed())) {
-    }
-    else {
-        await navMenu.profileIcon.click();
-    }
- });
+/**
+ * Teardown Section
+ *
+ * steps for consistent tear down once feature execution is complete
+ */
 
 /**
  * Navigations
@@ -105,42 +40,114 @@ let MAX_TIME_WAIT = 10000;
  * shared navigations to important pages
  */
 
-Given('User is in Saves page', async() => {
-    const savesPageInfo = await getPageInfo('saves');
-    const currentUrl = await browser.getCurrentUrl();
+/**
+ * Common Actions? / Common Accounts?
+ *
+ * things like save a vehicle, create a deal, etc.
+ */
 
-    const onSavesPage = savesPageInfo.urlTest.test(currentUrl);
+async function hasNotPreviouslyLoggedIn() {
+  await browser.driver.sleep(15 * 1000);
+  return browser.driver.getCurrentUrl().then((url) => {
+    return url.includes("account.toyota.com");
+  });
+}
 
-    if(!onSavesPage){
-        const savesPage = constructSavePageUrl();
-        await browser.driver.get(savesPage);
+When(/User Signs In(\s\"(.*?)\")?(\s\"(.*?)\")?/, async (email?: string, password?: string) => {
+    await browser.driver.wait(
+      until.visibilityOf(navMenu.profileIcon),
+      MAX_TIME_WAIT,
+      "Top Nav Profile Icon taking too long to appear in the DOM"
+    );
+    navMenu.profileIcon.click();
+    await browser.driver.wait(
+      until.visibilityOf(navMenu.dgComponentMenuDropdownDesktop),
+      MAX_TIME_WAIT,
+      "Dropdown Element taking too long to appear in the DOM"
+    );
+    await navMenu.dgLoginButton.click();
+    if (await hasNotPreviouslyLoggedIn()) {
+      await createAccountPage.userName.sendKeys(
+        email ? email : browser.params.caemailreg
+      );
+      await createAccountPage.nextStepButton.click();
+      await createAccountPage.userPwd.sendKeys(
+        password ? password : browser.params.capwdreg
+      );
+      await createAccountPage.signInButton.click();
     }
+  }
+);
 
-    await waitForVisibilityOf(savesPageInfo.pageDef, savesPageInfo.title);
+Given("User is not logged in to account", async () => {
+  await browser.driver.wait(
+    until.visibilityOf(navMenu.profileIcon),
+    MAX_TIME_WAIT,
+    "Top Nav Profile Icon taking too long to appear in the DOM"
+  );
+  navMenu.profileIcon.click();
+
+  await browser.driver.wait(
+    until.visibilityOf(navMenu.dgComponentMenuDropdownDesktop),
+    MAX_TIME_WAIT,
+    "Dropdown Element taking too long to appear in the DOM"
+  );
+  if (!Assertion.expect(await navMenu.dgLoginButton.isDisplayed())) {
+  } else {
+    await navMenu.profileIcon.click();
+  }
 });
 
-When('User loads the Saves page', async () => {
-    await waitForVisibilityOf(navMenu.profileIcon, 'Top Nav Profile Icon')
-    await navMenu.profileIcon.element(by.xpath('//*[@id="dg-component-nav-menu-desktop"]/div[1]/img')).click();
+/**
+ * Navigations
+ *
+ * shared navigations to important pages
+ */
 
-    // Click Saves Linkout, Check the Url, and then Navigate Back
-    browser.driver.wait(until.visibilityOf(navMenu.dgComponentMenuDropdownDesktop),MAX_TIME_WAIT,'Dropdown Element taking too long to appear in the DOM');
-    if( await navMenu.savesPageLinkOut.isDisplayed() == false){
-        navMenu.profileIcon.click();
-    }
-    await navMenu.dgComponentMenuDropdownDesktop.$('#dg-menu-saves-page-linkout').click();
-    await browser.driver.sleep(MAX_TIME_WAIT);
-    await waitForVisibilityOf(savesPage.savePageTitle, 'Saves Page');
+Given("User is in Saves page", async () => {
+  const savesPageInfo = await getPageInfo("saves");
+  const currentUrl = await browser.getCurrentUrl();
+
+  const onSavesPage = savesPageInfo.urlTest.test(currentUrl);
+
+  if (!onSavesPage) {
+    const savesPage = constructSavePageUrl();
+    await browser.driver.get(savesPage);
+  }
+
+  await waitForVisibilityOf(savesPageInfo.pageDef, savesPageInfo.title);
 });
 
-Then(/User is redirected to \"(.*?)\" Page/, async(page: string) => {
-    const pageInfo = await getPageInfo(page.toLowerCase());
+When("User loads the Saves page", async () => {
+  await waitForVisibilityOf(navMenu.profileIcon, "Top Nav Profile Icon");
+  await navMenu.profileIcon
+    .element(by.xpath('//*[@id="dg-component-nav-menu-desktop"]/div[1]/img'))
+    .click();
 
-    await waitForVisibilityOf(pageInfo.pageDef, pageInfo.title);
-    const currentUrl = await browser.driver.getCurrentUrl();
-    expect(pageInfo.urlTest.test(currentUrl));
+  // Click Saves Linkout, Check the Url, and then Navigate Back
+  browser.driver.wait(
+    until.visibilityOf(navMenu.dgComponentMenuDropdownDesktop),
+    MAX_TIME_WAIT,
+    "Dropdown Element taking too long to appear in the DOM"
+  );
+  if ((await navMenu.savesPageLinkOut.isDisplayed()) == false) {
+    navMenu.profileIcon.click();
+  }
+  await navMenu.dgComponentMenuDropdownDesktop
+    .$("#dg-menu-saves-page-linkout")
+    .click();
+  await browser.driver.sleep(MAX_TIME_WAIT);
+  await waitForVisibilityOf(savesPage.savePageTitle, "Saves Page");
+});
 
-    expect(await pageInfo.pageDef.isDisplayed());
+Then(/User is redirected to \"(.*?)\" Page/, async (page: string) => {
+  const pageInfo = await getPageInfo(page.toLowerCase());
+
+  await waitForVisibilityOf(pageInfo.pageDef, pageInfo.title);
+  const currentUrl = await browser.driver.getCurrentUrl();
+  expect(pageInfo.urlTest.test(currentUrl));
+
+  expect(await pageInfo.pageDef.isDisplayed());
 });
 
 /**
@@ -151,25 +158,26 @@ Then(/User is redirected to \"(.*?)\" Page/, async(page: string) => {
  */
 
 Given(/User is in tier \"(.*?)\" Saves page/, async (tierNo: string) => {
-    const tierXSavesPageUrl = await constructSavePageUrl(browser.params[`tier${tierNo}url`]);
-    await browser.driver.get(tierXSavesPageUrl);
+  const tierXSavesPageUrl = await constructSavePageUrl(
+    browser.params[`tier${tierNo}url`]
+  );
+  await browser.driver.get(tierXSavesPageUrl);
 });
 
-Given('User is not logged in', async () => {
-    //user is logged out by default
-    //do nothing
-    //TODO: add validation and potential log out flow from WILL
+Given("User is not logged in", async () => {
+  //user is logged out by default
+  //do nothing
+  //TODO: add validation and potential log out flow from WILL
 });
 
-Given('User is on desktop', async () => {
-    browser.driver.manage().window().maximize();
+Given("User is on desktop", async () => {
+  browser.driver.manage().window().maximize();
 });
 
-Given('User is on tablet', async () => {
-    browser.driver.manage().window().setSize(768, 1024);
+Given("User is on tablet", async () => {
+  browser.driver.manage().window().setSize(768, 1024);
 });
 
-Given('User is on mobile', async () => {
-    browser.driver.manage().window().setSize(375, 667);
+Given("User is on mobile", async () => {
+  browser.driver.manage().window().setSize(375, 667);
 });
-
